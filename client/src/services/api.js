9fd46bdf150f5e6505507b75431341ab4eb16327
subhashcherukuri <subhashcherukuri1729@gmail.com>
2026-09-11 18:01:58 +0530

    Set default backend URL to user's Render API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://expense-tracker-api-jvzj.onrender.com/api",
+    "https://expense-tracker-api-qyti.onrender.com/api",
 });
 
 export default api;
